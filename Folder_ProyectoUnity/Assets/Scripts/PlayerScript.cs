85f52f5553654c353543a58ee268ac92ac00a089
AdrianAndia99 <151102699+AdrianAndia99@users.noreply.github.com>
2024-05-29 19:27:32 -0500

    de ahi sigo

--- Folder_ProyectoUnity/Assets/Scripts/PlayerScript.cs
+++ Folder_ProyectoUnity/Assets/Scripts/PlayerScript.cs
@@ -5,35 +5,49 @@ using UnityEngine.InputSystem;
 
 public class PlayerScript : MonoBehaviour
 {
-    //private Vector2 movimiento;
-    private Animator animator;
-    private Rigidbody rb;
+    PlayerInput playerInput;
+    InputAction moveAction;
 
-    public float velocidadMovimiento = 3.0f;
-    public float velocidadRotacion = 200.0f;
-    public float x, y;
-
+    public float moveSpeed;
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        animator = GetComponent<Animator>();
+        playerInput = GetComponent<PlayerInput>();
+        moveAction = playerInput.actions.FindAction("Movimiento");
     }
 
-   /* public void OnMove(InputAction.CallbackContext context)
+    void Move()
     {
-        movimiento = context.ReadValue<Vector2>();
-    }*/
-
-    void Update()
-    {
-
-        x = Input.GetAxis("Horizontal");
-        y = Input.GetAxis("Vertical");
-
-        transform.Rotate(0, x * Time.deltaTime * velocidadRotacion, 0);
-        transform.Translate(0,0,y * Time.deltaTime * velocidadMovimiento);
-
-        animator.SetFloat("VelX",x);
-        animator.SetFloat("VelY", y);
+        Vector2 direction = moveAction.ReadValue<Vector2>();
     }
+    /* //private Vector2 movimiento;
+     private Animator animator;
+     private Rigidbody rb;
+
+     public float velocidadMovimiento = 3.0f;
+     public float velocidadRotacion = 200.0f;
+     public float x, y;
+
+     void Start()
+     {
+         rb = GetComponent<Rigidbody>();
+         animator = GetComponent<Animator>();
+     }
+
+    /* public void OnMove(InputAction.CallbackContext context)
+     {
+         movimiento = context.ReadValue<Vector2>();
+     }*/
+
+    /* void Update()
+     {
+
+         x = Input.GetAxis("Horizontal");
+         y = Input.GetAxis("Vertical");
+
+         transform.Rotate(0, x * Time.deltaTime * velocidadRotacion, 0);
+         transform.Translate(0,0,y * Time.deltaTime * velocidadMovimiento);
+
+         animator.SetFloat("VelX",x);
+         animator.SetFloat("VelY", y);
+     }*/
 }

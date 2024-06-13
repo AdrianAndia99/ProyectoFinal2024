690a4db91d2501ec4ee2c17d8732d667202433b6
AdrianAndia99 <151102699+AdrianAndia99@users.noreply.github.com>
2024-06-13 10:50:50 -0500

    pusheo

--- Folder_ProyectoUnity/Assets/Scripts/SceneManage.cs
+++ Folder_ProyectoUnity/Assets/Scripts/SceneManage.cs
@@ -3,12 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class SceneManage : MonoBehaviour
 {
+
     public void LoadScene(string nameScene)
     {
         SceneManager.LoadScene(nameScene);
+
     }
 
     public void Salir()

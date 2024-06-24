a2ce472792a2b00b50d7f5dceacf539835d5210d
AdrianAndia99 <151102699+AdrianAndia99@users.noreply.github.com>
2024-06-23 19:57:04 -0500

    perro mueve

--- Folder_ProyectoUnity/Assets/Scripts/DogScript.cs
+++ Folder_ProyectoUnity/Assets/Scripts/DogScript.cs
@@ -1,8 +1,56 @@
-using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class DogScript : MonoBehaviour
 {
+    public Transform[] points;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+    private bool forward = true;
 
+    void Start()
+    {
+        SphereCollider detectionArea = gameObject.AddComponent<SphereCollider>();
+
+        agent = GetComponent<NavMeshAgent>();
+
+        agent.autoBraking = false;
+
+        GotoNextPoint();
+    }
+    void GotoNextPoint()
+    {
+        if (points.Length == 0)
+        {
+            return;
+        }
+
+        agent.destination = points[destPoint].position;
+
+        if (forward)
+        {
+            destPoint++;
+            if (destPoint >= points.Length)
+            {
+                destPoint = points.Length - 1;
+                forward = false;
+            }
+        }
+        else
+        {
+            destPoint--;
+            if (destPoint < 0)
+            {
+                destPoint = 0;
+                forward = true;
+            }
+        }
+    }
+    void Update()
+    {
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            GotoNextPoint();
+        }
+    }
 }

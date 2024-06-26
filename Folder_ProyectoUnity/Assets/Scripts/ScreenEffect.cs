5172a71efc6ade36c74bb41de10f1dc29aff586e
AdrianAndia99 <151102699+AdrianAndia99@users.noreply.github.com>
2024-06-25 21:33:51 -0500

    profe bromita XD

--- Folder_ProyectoUnity/Assets/Scripts/ScreenEffect.cs
+++ Folder_ProyectoUnity/Assets/Scripts/ScreenEffect.cs
@@ -5,21 +5,10 @@ using UnityEngine;
 
 public class ScreenEffect : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 1f;
-
-    [SerializeField] private float offset;
-
-    private Vector2 startPosition;
-
-    private float newXposition;
-    private void Start()
-    {
-        startPosition = transform.position;
-    }
+    private float moveSpeed = 10f;
 
     private void Update()
     {
-        newXposition = Mathf.Repeat(Time.time * -moveSpeed, offset);
-        transform.position = startPosition + Vector2.right * newXposition;
+        RenderSettings.skybox.SetFloat("_Rotation",Time.time*moveSpeed);
     }
 }
